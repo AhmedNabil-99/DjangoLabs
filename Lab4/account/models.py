6db337dc9a3e0753c83de7c3548498758222a5ac
AhmedNabil-99 <an63805@gmail.com>
2024-08-29 02:42:31 +0300

    Lab4

---
+++ Lab4/account/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Account(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    traineeobj = models.ForeignKey("trainee.Trainee", on_delete=models.CASCADE)
